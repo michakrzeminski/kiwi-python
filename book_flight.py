6153881fec320debd02c5e449f23688d1d420b44
Riva <rivanathans@gmail.com>
2018-02-11 14:03:31 +0100

    refactor

--- book_flight.py
+++ book_flight.py
@@ -6,71 +6,11 @@ import json
 import requests
 import sys
 
-# get options from user
-parser = argparse.ArgumentParser()
-parser.add_argument('--date', help='departure date', required=True)
-parser.add_argument('--from-location', help='from (airport code)', required=True)
-parser.add_argument('--to-location', help='to (airport code), omit to see flights to anywhere', required=False)
-parser.add_argument('--return-length', help='return flight, days in destination', type=int, required=False)
-parser.add_argument('--one-way', help='one-way flight (default)', action='store_true', required=False)
-parser.add_argument('--cheapest', help='find cheapest flight (default)', action='store_true', required=False)
-parser.add_argument('--fastest', help='find fastest flight', action='store_true', required=False)
-parser.add_argument('--bags', help='number of bags', type=int, required=False)
-
-args = parser.parse_args()
-print(args)
-
-# search flights
-search_url = 'https://api.skypicker.com/flights'
-
-search_params = {
-	'v': 3,
-	'typeFlight': 'oneway',
-	'adults': 1,
-	'sort': 'price',
-	'limit': 1
-}
-
-# fill in location -- from
-search_params['flyFrom'] = args.from_location
-
-# fill in location -- to
-if args.to_location:
-	search_params['to'] = args.to_location
-
-# fill in departure date
-date_in = datetime.strptime(args.date, '%d-%m-%Y')
-date_out = date_in.strftime('%d/%m/%Y')
-search_params['dateFrom'] = date_out
-search_params['dateTo'] = date_out
-
-# fill in return / stay length
-if args.return_length:
-	search_params['typeFlight'] = 'return'
-	search_params['daysInDestinationFrom'] = args.return_length
-	search_params['daysInDestinationTo'] = args.return_length
-
-# find fastest instead of cheapest
-if args.fastest:
-	search_params['sort'] = 'duration'
-
-print(search_params)
-
-response = requests.get(search_url, params=search_params)
-print(response.status_code)
-print(response.content)
-
-# error -- search request failed
-if response.status_code >= 400:
-	sys.exit('search request failed')
-
-# error -- no flights matching search criteria
-if response.json().get('_results') == 0:
-	sys.exit('no flights found matching search criteria')
+SEARCH_URL = 'https://api.skypicker.com/flights'
+BOOKING_URL = 'http://128.199.48.38:8080/booking'
 
 '''
 # book flight
-booking_url = 'http://128.199.48.38:8080/booking'
 booking_body = {
 	'currency': 'USD',
 	'bags': 2,
@@ -93,3 +33,67 @@ print(booking_response.status_code)
 print(booking_response.headers['content-type'])
 print(json.loads(booking_response.content))
 '''
+
+# get options from command-line arguments
+def parse_arguments():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--date', help='departure date', required=True)
+	parser.add_argument('--from-location', help='from (airport code)', required=True)
+	parser.add_argument('--to-location', help='to (airport code), omit to see flights to anywhere', required=False)
+	parser.add_argument('--return-length', help='return flight, days in destination', type=int, required=False)
+	parser.add_argument('--one-way', help='one-way flight (default)', action='store_true', required=False)
+	parser.add_argument('--cheapest', help='find cheapest flight (default)', action='store_true', required=False)
+	parser.add_argument('--fastest', help='find fastest flight', action='store_true', required=False)
+	parser.add_argument('--bags', help='number of bags', type=int, required=False)
+	return parser.parse_args()
+
+# search flights
+def search_flights(args):
+
+	# initialize search parameters
+	search_params = {
+	'v': 3,
+	'typeFlight': 'oneway',
+	'adults': 1,
+	'sort': 'price',
+	'limit': 1
+	}
+
+	# fill in location -- from
+	search_params['flyFrom'] = args.from_location
+
+	# fill in location -- to
+	if args.to_location:
+		search_params['to'] = args.to_location
+
+	# fill in departure date
+	date_in = datetime.strptime(args.date, '%d-%m-%Y')
+	date_out = date_in.strftime('%d/%m/%Y')
+	search_params['dateFrom'] = date_out
+	search_params['dateTo'] = date_out
+
+	# fill in return / stay length
+	if args.return_length:
+		search_params['typeFlight'] = 'return'
+		search_params['daysInDestinationFrom'] = args.return_length
+		search_params['daysInDestinationTo'] = args.return_length
+
+	# option -- find fastest
+	if args.fastest:
+		search_params['sort'] = 'duration'
+
+	# use Kiwi search API
+	response = requests.get(SEARCH_URL, params=search_params)
+
+	# handle errors
+	if response.status_code >= 400:
+		sys.exit('search request failed')
+	if response.json().get('_results') == 0:
+		sys.exit('no flights found matching search criteria')
+
+	print(search_params)
+	print(response.json())
+	return response.json()
+
+args = parse_arguments()
+response = search_flights(args)
