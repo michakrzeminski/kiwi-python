6398328da5ef9fd93cc415043c4b51da3491f96a
Riva <rivanathans@gmail.com>
2018-02-11 15:38:18 +0100

    print booking confimration at the end

--- book_flight.py
+++ book_flight.py
@@ -9,31 +9,6 @@ import sys
 SEARCH_URL = 'https://api.skypicker.com/flights'
 BOOKING_URL = 'http://128.199.48.38:8080/booking'
 
-'''
-# book flight
-booking_body = {
-	'currency': 'USD',
-	'bags': 2,
-	'booking_token': 'b/n57BoTjASi2ByXlxiZP3oDvlzrQBO2CyfmSlMWf797iyUPg9g6AUmQTRMN0LUSLGZwJU1Lf1p3eAZK1HMA49WbjkNwLkAzmc46t627Q2+vu5rOy5FEWfc0SPMohsJpkDhtZlR5sk91ew3DkQ6bcSu09yILirZcDVNilLZ724at+JExC1oNgH8W1Uv4p7vB52NnOZd0HvFLGjXvjpRcKzI/VZqVEEGNQaDsXYjldG5kLKYEE/Eaq3EwZUVxidUiBd5RpxK1BUe4nxPLvek+5OD6dhdnEMlHaMCsK7fol/a/BopzHz/kXjV54+UuDfNr1gl7FpAQHKpwhI2YVSP/wHIkVD9YnGwPdU2cd5z956j6ncGW+/Fmmpne0OzsGsLOnhinINS5jujKehoItfB3h0Hcp0ggilMUht+FMuIJcm7BcixRmOshfqChkwiEMlJTzEF4K1vPlWX3V3giu4Ak0wnoHx72IJ6EZf7XxGwp55SrdKeyh4x/0iPodez3sCubsM4ckPKx414mFEHtrct034C8NWfDfpMiIdS7qe7U3xB/oYRZb5fct285RsbnWcFjNHW+UdCf8b7C5yPZwX5EPsudYMRvh+NUr4YeLOnel6wGENowwfZ4bV/t02z4LFahj58C0sMTDktcvljeeAke6Tuvd9kiRzibyq43vcm4fdPjnAD+UKyjmshOn7QOu99sfmFIYhdYxdfGjuTOXZgQUg==',
-	'passengers': [
-		{
-			'firstName': 'Riva',
-			'lastName': 'Nathans',
-			'title': 'Mrs',
-			'email': 'rivanathans@gmail.com',
-			'documentID': 'id',
-			'birthday': '1988-07-12'
-		}
-	]
-}
-
-booking_response = requests.post(booking_url, json=booking_body)
-
-print(booking_response.status_code)
-print(booking_response.headers['content-type'])
-print(json.loads(booking_response.content))
-'''
-
 # get options from command-line arguments
 def parse_arguments():
 	parser = argparse.ArgumentParser()
@@ -93,9 +68,49 @@ def search_flights(args):
 
 	return response.json()
 
+# book flight
 def book_flight(args, response):
-	print(response.get('data')[0].get('booking_token'))
+
+	# initialize booking request
+	booking_body = {
+		'currency': 'USD',
+		'bags': 0,
+		'passengers': [
+			{
+				'firstName': 'Riva',
+				'lastName': 'Nathans',
+				'title': 'Mrs',
+				'email': 'rivanathans@gmail.com',
+				'documentID': 'id_string',
+				'birthday': '1988-07-12'
+			}
+		]
+	}
+
+	# add booking token
+	booking_body['booking_token'] = response.get('data')[0].get('booking_token')
+
+	# add bags, if any
+	if args.bags:
+		booking_body['bags'] = args.bags
+
+	# use Kiwi fake booking API
+	response = requests.post(BOOKING_URL, json=booking_body)
+
+	# handle errors
+	if response.status_code >= 400:
+		sys.exit('booking request failed')
+	if response.json().get('pnr') == None:
+		sys.exit('failed to confirm reservation')
+
+	print(booking_body)
+	print(response.status_code)
+	print(response.headers['content-type'])
+	print(response.json())
+
+	return response.json().get('pnr')
 
 args = parse_arguments()
 response = search_flights(args)
-book_flight(args, response)
+confirmation = book_flight(args, response)
+print('booking confirmation: ' + confirmation)
